a7c776a11db73a5ebaab12236a4169227131d8ad
Arthur Dantas <arthurdantaspro@gmail.com>
2023-05-11 14:41:00 -0300

    implementação inicial dos dados de técnicos

--- src/app/components/tecnicos/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnicos/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {Tecnico} from "../../../models/tecnico";
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+const ELEMENT_DATA: Tecnico[] = [
+  {
+    id: 1,
+    nome: 'arthur',
+    cpf: '123.123.123-12',
+    email: "arthur@mail.com",
+    senha: '123456',
+    perfis: ['0'],
+    dataCriacao: '11/05/2023'
+  }
+];
